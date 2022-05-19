a218eda5dbff7cb5017ddd787f81ba19546bc11b
kydculx <kydculx@gmail.com>
2022-05-19 17:01:17 +0900

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "73193f7ccf26feaa63df013ed3745fa1",
-"index.html": "1c2cd6bccd15f3488f3d2111f1de06d8",
-"/": "1c2cd6bccd15f3488f3d2111f1de06d8",
-"main.dart.js": "39331bc45cb8c235862b41f2baf59858",
+"index.html": "70c0da4abc9006f48da458d86d22e38b",
+"/": "70c0da4abc9006f48da458d86d22e38b",
+"main.dart.js": "45ad324d617f9f420328880e465ed04e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
